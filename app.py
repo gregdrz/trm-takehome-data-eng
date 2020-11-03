b3189ee3b5c866274fcadbc631133e7b707c1e35
Greg Drzadzewski <gregskyd@gmail.com>
2020-11-03 03:06:33 -0800

    added time measure

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import psycopg2
 from flask import Flask, request
 from google.cloud import bigquery
 from google_auth_oauthlib import flow
+import time
+
 
 conn = None
 client = None
@@ -105,6 +107,8 @@ def fetch():
 @app.route('/address/exposure/direct',  methods=['GET'])
 def address_exposure_direct():
     global conn
+    time_before = time.time()
+
     address = request.args.get('address', None)
     start_date = request.args.get('startdate', '0001-01-01T00:00:00Z')
     end_date = request.args.get('enddate', '9999-12-31T23:59:59Z')
@@ -122,77 +126,77 @@ def address_exposure_direct():
         output_ordering = "outflow"
 
     query_string = f"""
-  WITH outflow_data AS (
-  SELECT
-    sender,
-    receiver,
-    sum(total_value) as total_value
-  FROM
-    daily_transfers
-  WHERE
-    (sender = '{address}' OR receiver = '{address}') AND
-    date >= '{start_date}' AND
-    date <= '{end_date}'
-  GROUP BY
-    sender,
-    receiver
-  ORDER BY
-    total_value desc
-  ),
+      WITH outflow_data AS (
+      SELECT
+        sender,
+        receiver,
+        sum(total_value) as total_value
+      FROM
+        daily_transfers
+      WHERE
+        (sender = '{address}' OR receiver = '{address}') AND
+        date >= '{start_date}' AND
+        date <= '{end_date}'
+      GROUP BY
+        sender,
+        receiver
+      ORDER BY
+        total_value desc
+      ),
 
-  outflow_table AS (
-  SELECT
-    od.sender as address_of_interest,
-    od.receiver as counterpart,
-    od.total_value as outflow,
-    0 as inflow,
-    od.total_value as total_flow
-  FROM
-    outflow_data as od
-  WHERE od.sender= '{address}'
-  ),
-  inflow_table AS (
-  SELECT
-    od.receiver as address_of_interest,
-    od.sender as counterpart,
-    od.total_value as inflow,
-    0 as outflow,
-    od.total_value as total_flow
-  FROM
-    outflow_data as od
-  WHERE od.receiver= '{address}'
-  )
+      outflow_table AS (
+      SELECT
+        od.sender as address_of_interest,
+        od.receiver as counterpart,
+        od.total_value as outflow,
+        0 as inflow,
+        od.total_value as total_flow
+      FROM
+        outflow_data as od
+      WHERE od.sender= '{address}'
+      ),
+      inflow_table AS (
+      SELECT
+        od.receiver as address_of_interest,
+        od.sender as counterpart,
+        od.total_value as inflow,
+        0 as outflow,
+        od.total_value as total_flow
+      FROM
+        outflow_data as od
+      WHERE od.receiver= '{address}'
+      )
 
-  SELECT
-    address_of_interest,
-    counterpart,
-    sum(d.outflow) as outflow,
-    sum(d.inflow) as inflow,
-    sum(d.total_flow) as totalflow
-  FROM (
-    SELECT
-      address_of_interest,
-      counterpart,
-      outflow,
-      inflow,
-      total_flow
-    FROM inflow_table
-      UNION ALL
-    SELECT
-      address_of_interest,
-      counterpart,
-      outflow,
-      inflow,
-      total_flow FROM outflow_table
-    ) as d
-  GROUP BY
-    address_of_interest,
-    counterpart
-  order by
-    {output_ordering} desc
-  LIMIT {limit}
-  OFFSET {offset}
-  """
+      SELECT
+        address_of_interest,
+        counterpart,
+        sum(d.outflow) as outflow,
+        sum(d.inflow) as inflow,
+        sum(d.total_flow) as totalflow
+      FROM (
+        SELECT
+          address_of_interest,
+          counterpart,
+          outflow,
+          inflow,
+          total_flow
+        FROM inflow_table
+          UNION ALL
+        SELECT
+          address_of_interest,
+          counterpart,
+          outflow,
+          inflow,
+          total_flow FROM outflow_table
+        ) as d
+      GROUP BY
+        address_of_interest,
+        counterpart
+      order by
+        {output_ordering} desc
+      LIMIT {limit}
+      OFFSET {offset}
+      """
 
     cur = conn.cursor()
     cur.execute(query_string)
@@ -202,5 +206,9 @@ def address_exposure_direct():
                     "inflows": i,
                     "outflows": o,
                     "total_flows": t})
+
+    time_after = time.time()
+    difference = float(time_after - time_before)
+    print(f'time={difference}')
 
     return {"data": out, "success": True}
